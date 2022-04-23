74c286e3b23269195babc643149a74ae90de555c
Lukas Protiva <luky.protiva@gmail.com>
2022-04-23 10:54:10 +0200

    add endpoint for specific sources

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const PORT = 8080;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express');
 const app = express();
 
 const newspapers = [
@@ -48,6 +49,34 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
     res.json(articles);
+});
+
+app.get('/news/:newspaperId', async (req, res) => {
+    const newspaperId = req.params.newspaperId;
+
+    const newspaperAdddress = newspapers.filter((newspaper) => newspaper.name == newspaperId)[0].address;
+    const newspaperBase = newspapers.filter((newspaper) => newspaper.name == newspaperId)[0].base;
+
+    axios
+        .get(newspaperAdddress)
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const specificArticles = [];
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text();
+                const url = $(this).attr('href');
+
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId,
+                });
+            });
+            res.json(specificArticles);
+        })
+        .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
